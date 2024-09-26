d3a5f886721e5e6101b49976b2ddb256770f60cf
Hamel Desai <hameldesai93@gmail.com>
2024-09-26 15:41:32 -0400

    Finish Styling with StyleX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import PRODUCTS from "./products";
 import { ProductCard } from "./ProductCard";
 import styles from "./page.module.css";
+import * as stylex from "@stylexjs/stylex";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-[960px] flex flex-wrap">
+    <main {...stylex.props(s.main)}>
       {PRODUCTS.map((product) => (
-        <div key={product.id} className={styles.card}>
+        <div key={product.id} {...stylex.props(s.card)}>
           <ProductCard product={product} />
         </div>
       ))}
     </main>
   );
 }
+const MOBILE = "@media screen and (max-width: 768px)";
+
+const s = stylex.create({
+  main: {
+    margin: "0 auto",
+    maxWidth: 960,
+    fontFamily: "sans-serif",
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  card: {
+    padding: "0.2rem",
+    width: {
+      default: "33%",
+      [MOBILE]: "100%",
+    },
+    maxWidth: {
+      default: "33%",
+      [MOBILE]: "100%",
+    },
+  },
+});
